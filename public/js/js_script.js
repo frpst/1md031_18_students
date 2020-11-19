cda08d97352daae58978726094c08bebeab59c39
frpst <fredrik.starback@gmail.com>
2020-11-19 13:39:35 +0100

    Meny inladdad genom js
    Signed-off-by: frpst <fredrik.starback@gmail.com>

---
+++ public/js/js_script.js
@@ -0,0 +1,16 @@
+function MenuItem(name,kcal,gluten,lactose,image) {
+    this.image = function(){
+	return image}
+    this.name = function(){
+	return "name"}
+    this.name = function(){
+	return name};
+    this.kcal = function(){
+	return kcal};
+    this.gluten = function(){
+	return gluten};
+    this.lactose = function() {
+	return lactose;}}
+
+
+
---
+++ public/js/vue_script.js
@@ -0,0 +1,21 @@
+
+var vm = new Vue({
+  el: '#myID',
+  data: {
+    arbitraryVariableName: 'Välj en burgare'
+  }
+})
+
+var vm = new Vue({
+    el: '#meny',
+    data: {
+	menu : [new MenuItem("McBurg",3650,true,true,"https://upload.wikimedia.org/wikipedia/commons/9/9a/Big_Mac_hamburger.jpg"),new MenuItem("BorgyTime",232,true,true,"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg"),new MenuItem("Feaster",23,false,false,"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/716px-NYC-Diner-Bacon-Cheeseburger.jpg")]
+    }
+})
+
+
+
+		 
+
+
+    
