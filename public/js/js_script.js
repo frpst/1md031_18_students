f967bee1163ff277b1735ebd78a6286a7011c7c6
frpst <fredrik.starback@gmail.com>
2020-12-19 15:26:31 +0100

    nu kommer det man fyllt i form:et upp längst ner när man trycker på order

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,4 +1,4 @@
-function MenuItem(name,kcal,gluten,lactose,image) {
+/*function MenuItem(name,kcal,gluten,lactose,image) {
     this.image = function(){
 	return image}
     this.name = function(){
@@ -11,6 +11,60 @@ function MenuItem(name,kcal,gluten,lactose,image) {
 	return gluten};
     this.lactose = function() {
 	return lactose;}}
+*/
+
+
+/*function valueReader() {
+    var theArray = []
+    var inputs = document.querySelectorAll("form");
+    var length_input = document.getElementById("form").elements.length
+
+    for (var i=0;i<length_input;i++){
+	var svar = document.getElementById("form").elements[i];
+	if (svar.type!=="radio") {
+	    theArray.push(x.value)
+	}
+    }
+    theArray.push(document.querySelector('input[type="radio"]:checked').val());
+    
+
+    return theArray
+}
+    */
+	
+/*function valueReader(event) {
+	var name= document.getElementById('firstlastname').value
+	var email=document.getElementById('email').value
+	var payment=document.getElementById('payment').value
+	var sex =document.querySelector('input[name="gender"]:checked').value
+
+	var theArray= [name,email,payment,sex];
+	console.log(theArray)
+
+	return theArray;*/
+
+function valueReader () {
+    var detailArray = [];
+    var inputs = document.querySelectorAll("form");
+    var length_input = document.getElementById("form").elements.length;
+
+    for (var i=0;i<length_input;i++){
+	var svar = document.getElementById("form").elements[i];
+	if (svar.type!=="radio") {
+	    detailArray.push(svar.value);
+	}
+    }
+	detailArray.push(document.querySelector('input[type="radio"]:checked').value);
+    return detailArray;
+}
+    
+
+/*function tester() {
+    console.log('yoyoyoyoyo');
+    return array;
+}*/
+	
+
 
 
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,21 +1,45 @@
-
-var vm = new Vue({
-  el: '#myID',
-  data: {
-    arbitraryVariableName: 'Välj en burgare'
-  }
-})
-
-var vm = new Vue({
+/*var vm = new Vue({
     el: '#meny',
-    data: {
-	menu : [new MenuItem("McBurg",3650,true,true,"https://upload.wikimedia.org/wikipedia/commons/9/9a/Big_Mac_hamburger.jpg"),new MenuItem("BorgyTime",232,true,true,"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg"),new MenuItem("Feaster",23,false,false,"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/716px-NYC-Diner-Bacon-Cheeseburger.jpg")]
+    data: {food}
+
+});
+
+
+/*var vm2 = new Vue({
+    el: '#order',
+    methods:{ valueReader: function(event){
+	 var theArray = []
+    var inputs = document.querySelectorAll("form");
+    var length_input = document.getElementById("form").elements.length
+
+    for (var i=0;i<length_input;i++){
+	var svar = document.getElementById("form").elements[i];
+	if (svar.type!=="radio") {
+	    theArray.push(svar.value)
+	}
     }
-})
+	theArray.push(document.querySelector('input[type="radio"]:checked').value);
+	console.log(theArray);
+	return theArray;
+    }},
+    
+    })*/
 
-
-
-		 
+var vm = new Vue({
+    el: '#themenu',
+    data: { customerDetails: [],
+            food},
+    methods: {
+        addOrder: function (event) {
+            this.customerDetails = valueReader();
+            console.log(customerDetails);
+        }
+    }
+            
+        
+        
+    
+});
 
 
     
