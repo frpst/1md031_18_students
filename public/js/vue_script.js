cf0287e65a4edd35225f5038813722d4e3aaa5a7
frpst <fredrik.starback@gmail.com>
2020-12-20 10:49:36 +0100

    allt utom kartan verkar funka

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -24,26 +24,68 @@
     }},
     
     })*/
+'use strict';
+var socket = io();
 
 var vm = new Vue({
     el: '#themenu',
+
     data: { customerDetails: [],
             food,
             checkBurgers: [],
-            hasordered: false},
+            hasordered: false,
+            count: 0,
+            ordernr: "",
+            orders: {}
+          },
+
     methods: {
+
         addOrder: function (event) {
             this.hasordered = true;
             this.customerDetails = valueReader();
-            console.log(customerDetails);
             
+            console.log(customerDetails);
+            var deliveryLocation = this.orders.T.details;
+            
+            socket.emit("addOrder", {details: deliveryLocation,
+                                     orderItems: this.checkBurgers,
+                                     customerInformation: this.customerDetails});
+            
+            
+
+            
+        },
+
+        
+        displayOrder: function (event) {
+            var T = {x: event.currentTarget.getBoundingClientRect().left,
+                     y: event.currentTarget.getBoundingClientRect().top
+                    };
+            this.orders = {
+                "T": {
+                    details: {x: event.clientX - 10 - T.x,
+                              y: event.clientY - 10 - T.y
+                             }
+                             
+                }
+            }; 
         }
     }
+});
+
+       
+            
+
+
+                                  
+            
+       
             
         
         
     
-});
+
 
 
     
