84d85cf0a7f14a57dec4d571756d560e6832e6d6
frpst <fredrik.starback@gmail.com>
2021-02-18 10:45:12 +0100

    addar det sista, tror jag

---
+++ public/menu.js
@@ -0,0 +1,38 @@
+var food = [
+
+    
+    { "name": "McBurg",
+  "kCal": 3650,
+  "lactose": true,
+  "gluten": false,
+      "img": "https://upload.wikimedia.org/wikipedia/commons/9/9a/Big_Mac_hamburger.jpg"
+    }
+    ,
+    {"name": "'BorgyTime!'",
+  "kCal": 232,
+  "lactose": true,
+  "gluten": false,
+     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg"
+    }
+    ,{
+    "name": "Feastere",
+  "kCal": 23,
+  "lactose": false,
+  "gluten": true,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/716px-NYC-Diner-Bacon-Cheeseburger.jpg"
+    },
+    {
+    "name": "Dankey Kang Berger",
+  "kCal": 2322,
+  "lactose": true,
+  "gluten": true,
+    "img": "https://twisper.com/wp-content/uploads/2020/03/close-up-photo-of-burger-3915906-scaled.jpg"
+    },
+    {
+    "name": "Hoola Bandoola burger",
+  "kCal": 12,
+  "lactose": false,
+  "gluten": false,
+    "img": "https://www.burgerdudes.se/wp-content/uploads/2020/02/prime_mikescheese_19feb_med.jpg"
+}
+]
